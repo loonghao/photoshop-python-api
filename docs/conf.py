3083becd831c80add4e6fdf0bda1c033155e34b5
hao.long <hoolongvfx@gmail.com>
2019-01-06 13:06:04 +0800

    autodoc_mock_imports

--- docs/conf.py
+++ docs/conf.py
@@ -183,3 +183,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+autodoc_mock_imports = ['_winreg']
