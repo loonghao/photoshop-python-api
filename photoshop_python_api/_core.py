9c15708b59a0cc537a45ad3e83be14e131a29432
hao.long <hoolongvfx@hotmail.com>
2020-02-15 15:40:01 +0800

    use pre-commit clear up code

--- photoshop_python_api/_core.py
+++ photoshop_python_api/_core.py
@@ -134,9 +134,9 @@ class Photoshop:
 
                 >>> from photoshop_python_api import Application
                 >>> app = Application()
-                >>> jsx = ("var doc = app.activeDocument;"
-                ...       "var orig_name = doc.name;"
-                ...       "alert(orig_name);")
+                >>> jsx = ('var doc = app.activeDocument;'
+                ...       'var orig_name = doc.name;'
+                ...       'alert(orig_name);')
                 >>> app.eval_javascript(jsx)
 
         """
