dbf72f03fea2c295f5070239cb1708369f5477a9
hao.long <hoolongvfx@hotmail.com>
2020-02-17 00:16:37 +0800

    Fix Build docs failed

--- docs/conf.py
+++ docs/conf.py
@@ -185,3 +185,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+
+autodoc_mock_imports = [
+    'winreg'
+]
