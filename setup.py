b75fb06f5a2b2ae82e46219936437d60f6fb4755
hao.long <hoolongvfx@hotmail.com>
2020-02-17 02:24:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@
 from setuptools import find_packages
 from setuptools import setup
 
-import photoshop
 
 setup(
     package_dir={'': 'src'},
     packages=find_packages('.'),
-    version=photoshop.__version__,
     setup_requires=["setuptools-scm", "setuptools>=40.0"],
     author_email='hal.long@outllok.com',
     install_requires=['comtypes==1.1.7'],
