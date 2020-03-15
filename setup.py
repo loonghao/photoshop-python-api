b09876ff981b9aa0a71e6433f22410d137c5c66f
hao.long <hoolongvfx@hotmail.com>
2020-03-15 21:16:02 +0800

    Fix wrong tag name

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: 6 - Beta',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: Microsoft :: Windows :: Windows 10',
