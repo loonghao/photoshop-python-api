c8a922cc1f0f0b57fd846e40059da5330e2b7a7a
hao.long <hoolongvfx@hotmail.com>
2020-04-07 23:53:42 +0800

    Add examples for save psd file

---
+++ examples/save_to_psd.py
@@ -0,0 +1,11 @@
+"""Save your current active document as a .psd file."""
+from photoshop import Session
+
+with Session() as ps:
+    psd_file = 'your/psd/save/file/path.psd'
+    doc = ps.active_document
+    options = ps.PhotoshopSaveOptions()
+    layers = doc.artLayers
+    doc.saveAs(psd_file, options, True)
+    ps.alert(f'alert("Task done!")')
+    ps.echo(doc.activeLayer)
