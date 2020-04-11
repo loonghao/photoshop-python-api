d4a291d9bc0232f778b92aff5b2fb3225372c420
hao.long <hoolongvfx@hotmail.com>
2020-04-11 10:18:07 +0800

    Improve example code

--- examples/create_thumbnail.py
+++ examples/create_thumbnail.py
@@ -1,20 +1,50 @@
+"""Create a thumbnail image for currently active document.
+
+You can use the thumbnail image to upload to Shotgun or Ftrack.
+
+"""
+
+# Import built-in modules
 import os
+from tempfile import mkdtemp
+
+# Import local modules
 from photoshop import Session
 
-with Session(auto_close=True) as ps:
-    orig_name = ps.active_document.name
-    width_str = ps.active_document.width
-    height_str = ps.active_document.height
-    thumb_name = f'{orig_name}_tumb'
-    index = width_str / 1248
 
-    thumb_width = int(width_str / index)
+def create_thumbnail(output_path=None, max_resolution=512):
+    """Create a thumbnail image for currently active document.
 
-    thumb_height = int(height_str / index)
+    Args:
+        output_path (str): The absolute output path of the thumbnail image.
+            The default is to output to a temporary folder.
+        max_resolution (int): The max resolution of the thumbnail. The default
+            is `512`.
 
-    thumb_doc = ps.active_document.duplicate(thumb_name)
-    thumb_doc.resizeImage(thumb_width, thumb_height - 100)
-    thumb_file = 'd:/thumb.jpg'
-    thumb_doc.saveAs(thumb_file, ps.JPEGSaveOptions(), asCopy=True)
-    thumb_doc.close()
-    os.startfile(thumb_file)
+    Returns:
+        str: The absolute output path of the thumbnail image.
+
+    """
+    output_path = output_path or os.path.join(mkdtemp(prefix="thumb"),
+                                              "thumb.jpg")
+
+    with Session(auto_close=True) as ps:
+        orig_name = ps.active_document.name
+        width_str = ps.active_document.width
+        height_str = ps.active_document.height
+        thumb_name = f"{orig_name}_thumb"
+
+        max_resolution = width_str / max_resolution
+        thumb_width = int(width_str / max_resolution)
+        thumb_height = int(height_str / max_resolution)
+
+        thumb_doc = ps.active_document.duplicate(thumb_name)
+        thumb_doc.resizeImage(thumb_width, thumb_height - 100)
+        thumb_doc.saveAs(output_path, ps.JPEGSaveOptions(), asCopy=True)
+        thumb_doc.close()
+        return output_path
+
+
+if __name__ == '__main__':
+    thumb_file = create_thumbnail()
+    print(f"Save thumbnail file to {thumb_file}.")
