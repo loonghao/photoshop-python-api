2c2497d35814fcef3fa4aab8f96507bcd43c620d
hao.long <hoolongvfx@hotmail.com>
2020-04-16 00:29:24 +0800

    Add unittest

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.yield_fixture
+def photoshop_app():
+    from photoshop.application import Application
+    app = Application()
+    app.documents.add(name="UnitTest")
+    yield app
+    app.activeDocument.close()
