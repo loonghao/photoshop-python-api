ff3d569aa39c384d327c110862e9beb25d3bd90f
hao.long <hoolongvfx@hotmail.com>
2020-04-16 00:31:51 +0800

    Update use Session

--- examples/color.py
+++ examples/color.py
@@ -1,25 +1,24 @@
-import photoshop as ps
+from photoshop import Session
 
-# Get photoshop instance.
-app = ps.Application()
-# Get current active document.
-doc = app.activeDocument
 
-# Add a solid color.
-textColor = ps.SolidColor()
-textColor.rgb.red = 43.0
-textColor.rgb.green = 197.0
-textColor.rgb.blue = 0.0
+with Session() as ps:
+    doc = ps.active_document
+    # Add a solid color.
+    textColor = ps.SolidColor()
+    textColor.rgb.red = 255.0
+    textColor.rgb.green = 197
+    textColor.rgb.blue = 255
 
-# Create empty layer.
-new_text_layer = doc.artLayers.add()
-# Set empty layer type to text layer
-new_text_layer.kind = ps.LayerKind.TextLayer
-# Set current text layer contents to "Hello, World!".
-new_text_layer.textItem.contents = 'Hello, World!'
-# Change current text layer position.
-new_text_layer.textItem.position = [160, 167]
-# Change current text layer text size.
-new_text_layer.textItem.size = 36
-# Change current text layer color.
-new_text_layer.textItem.color = textColor
+    # Create empty layer.
+    new_text_layer = doc.artLayers.add()
+    # Set empty layer type to text layer
+    new_text_layer.kind = ps.LayerKind.TextLayer
+    # Set current text layer contents to "Hello, World!".
+    new_text_layer.textItem.contents = 'Hello, World!'
+    # Change current text layer position.
+    new_text_layer.textItem.position = [160, 167]
+    # Change current text layer text size.
+    new_text_layer.textItem.size = 36
+    # Change current text layer color.
+    new_text_layer.textItem.color = textColor
+    assert new_text_layer.textItem.color.rgb.red == textColor.rgb.red
