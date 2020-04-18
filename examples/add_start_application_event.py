604c30929a1e4059f20d26bdf6e6962b3756f05e
hao.long <hoolongvfx@hotmail.com>
2020-04-19 01:01:02 +0800

    Add new example for add event

---
+++ examples/add_start_application_event.py
@@ -0,0 +1,19 @@
+"""Add event for Photoshop start application.
+
+In the current example, every time we start photoshop it will
+alert "Start Application Event".
+
+Just like you manually in Script> Script Events Manager to enable the event.
+
+"""
+
+from photoshop import Session
+from tempfile import mkdtemp
+import os
+
+with Session() as ps:
+    root = mkdtemp()
+    jsx_file = os.path.join(root, "event.jsx")
+    with open(jsx_file, "w") as f:
+        f.write('alert("Start Application event.")')
+    ps.app.notifiers.add(ps.event.Notify, jsx_file)
