99145ad12e4f73e216122febe4c456b6df6ff5d4
hao.long <hoolongvfx@hotmail.com>
2020-04-20 01:37:51 +0800

    Clear up

--- docs/generate_examples.py
+++ docs/generate_examples.py
@@ -1,9 +1,11 @@
-import os
 import glob
-from jinja2 import Template
-import stringcase
+import os
 
-template = Template(r"""
+import stringcase
+from jinja2 import Template
+
+template = Template(
+    r"""
 Examples
 ========
 {% for file_ in Examples.get_examples() %}
@@ -12,7 +14,8 @@ Examples
   .. literalinclude:: {{ Examples.convert_relative_path(file_) }}
 {% endfor %}
     
-""")
+"""
+)
 
 
 class Examples(object):
@@ -39,5 +42,4 @@ class Examples(object):
 
 root = os.path.dirname(os.path.dirname(__file__))
 with open("examples.rst", "w") as f:
-    f.write(template.render(
-        Examples=Examples(os.path.join(root, "examples"))))
+    f.write(template.render(Examples=Examples(os.path.join(root, "examples"))))
