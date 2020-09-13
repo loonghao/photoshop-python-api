5793e0eb9c42508778aff7bbe2a95909ec157c4a
hao.long <hoolongvfx@hotmail.com>
2020-09-13 17:26:15 +0800

    Fix wrong textItem type

--- examples/photoshop_session.py
+++ examples/photoshop_session.py
@@ -2,10 +2,10 @@
 
 import os
 from datetime import datetime
-
-from photoshop import Session
+from tempfile import mkdtemp
 
 import examples._psd_files as psd  # Import from examples.
+from photoshop import Session
 
 PSD_FILE = psd.get_psd_files()
 file_path = PSD_FILE["slate_template.psd"]
@@ -17,9 +17,9 @@ with Session(file_path, action="open", auto_close=True) as ps:
         "datetime": datetime.today().strftime("%Y-%m-%d"),
     }
     for layer in layer_set.layers:
-        if layer.kind == "TextLayer":
+        if layer.kind == ps.LayerKind.TextLayer:
             layer.textItem.contents = data[layer.textItem.contents.strip()]
 
-    jpg_file = "d:/photoshop_slate.jpg"
+    jpg_file = os.path.join(mkdtemp("photoshop-python-api"), "slate.jpg")
     ps.active_document.saveAs(jpg_file, ps.JPEGSaveOptions())
     os.startfile(jpg_file)
