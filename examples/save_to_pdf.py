9f13bf080900ac413964f4405ec0565666b0fc43
hao.long <hoolongvfx@hotmail.com>
2020-09-23 20:50:14 +0800

    Add new examples

---
+++ examples/save_to_pdf.py
@@ -0,0 +1,21 @@
+"""Save current active document as a PDF file."""
+import os
+from tempfile import mkdtemp
+
+from photoshop import Session
+
+with Session() as ps:
+    option = ps.PDFSaveOptions(jpegQuality=12,
+                               layers=True,
+                               view=True)
+    pdf = os.path.join(mkdtemp(), "test.pdf")
+    ps.active_document.saveAs(pdf, option)
+
+
+with Session() as ps:
+    option = ps.PDFSaveOptions()
+    option.jpegQuality = 12
+    option.layers = True
+    option.view = True
+    pdf = os.path.join(mkdtemp(), "test.pdf")
+    ps.active_document.saveAs(pdf, option)
