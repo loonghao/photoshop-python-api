72714a5c02101b2be3be04226fe50e9aa3844fed
hao.long <hoolongvfx@hotmail.com>
2020-10-22 23:19:40 +0800

    Update version

--- src/photoshop/__init__.py
+++ src/photoshop/__init__.py
@@ -3,7 +3,7 @@ from photoshop.session import Session
 __all__ = ["Session"]
 
 __title__ = "photoshop_python_api"
-__version__ = "0.13.0"
+__version__ = "0.14.0"
 __author__ = "Long Hao"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018 Long Hao"
