c5dd3cc39f090b5517a97b91a3227a4d27a87607
hao.long <hoolongvfx@hotmail.com>
2020-10-22 23:02:32 +0800

    Add two examples

---
+++ examples/cropping.py
@@ -0,0 +1,9 @@
+"""A cropping example."""
+
+from photoshop import Session
+
+with Session(action="new_document") as ps:
+    ps.active_document.trim(100, 12, 354, 246)
+    ps.active_document.crop(bounds=[100, 12, 354, 246],
+                            width=1920,
+                            height=1080)
---
+++ examples/trim.py
@@ -0,0 +1,10 @@
+"""A trim example."""
+
+import examples._psd_files as psd  # Import from examples.
+from photoshop import Session
+
+PSD_FILE = psd.get_psd_files()
+example_file = PSD_FILE["trim.psd"]
+with Session(example_file, action="open") as ps:
+    ps.active_document.trim(ps.TrimType.TopLeftPixel,
+                            True, True, True, True)
