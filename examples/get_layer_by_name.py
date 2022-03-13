ac1e209bc312956b095afadcea7e8fef56e74f1a
longhao <hal.long@outlook.com>
2022-03-13 14:46:49 +0800

    docs: add new example

---
+++ examples/get_layer_by_name.py
@@ -0,0 +1,13 @@
+# Import third-party modules
+import examples._psd_files as psd  # Import from examples.
+
+# Import local modules
+from photoshop import Session
+
+
+PSD_FILE = psd.get_psd_files()
+
+psd_file = PSD_FILE["export_layers_as_png.psd"]
+with Session(psd_file, action="open", auto_close=True) as ps:
+    art_layer = ps.active_document.artLayers.getByName("blue")
+    assert art_layer.name == "blue"
