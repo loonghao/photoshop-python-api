5957530674b9e394cd88953f181fb22401701d6b
longhao <hal.long@outlook.com>
2022-03-19 16:01:41 +0800

    docs: update docs settings

--- docs/gen_api_nav.py
+++ docs/gen_api_nav.py
@@ -14,12 +14,10 @@ def main():
     for path in sorted(Path(api_root).glob("**/*.py")):
         module_path = path.relative_to(root).with_suffix("")
         doc_path = path.relative_to(root).with_suffix(".md")
-        full_doc_path = Path(doc_path)
+        full_doc_path = Path("reference", doc_path)
         parts = list(module_path.parts)
         if parts[-1] == "__init__":
-            parts = parts[:-1]
-            doc_path = doc_path.with_name("index.md")
-            full_doc_path = full_doc_path.with_name("index.md")
+            continue
         elif parts[-1] == "__main__":
             continue
         elif parts[-1] == "__version__":
@@ -35,7 +33,7 @@ def main():
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.as_posix().replace("\\", "/"))
 
-    with mkdocs_gen_files.open("api.md", "w") as nav_file:
+    with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
         nav_file.writelines(nav.build_literate_nav())
 
 
