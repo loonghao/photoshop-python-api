d5350c89dbb9707eaab0e097e2d95fe1eb1e2d01
longhao <hal.long@outlook.com>
2022-07-17 11:59:22 +0800

    chore(examples): add new example
    add new example for convert Smart Object to ArtLayer

---
+++ examples/convert_smartobject_to_layer.py
@@ -0,0 +1,19 @@
+"""Convert Smart object to artLayer."""
+
+# Import local modules
+from photoshop import Session
+
+
+# example 1
+with Session() as ps:
+    js = """
+var idplacedLayerConvertToLayers = stringIDToTypeID( "placedLayerConvertToLayers" );
+executeAction( idplacedLayerConvertToLayers, undefined, DialogModes.NO );
+"""
+    ps.app.doJavaScript(js)
+
+# example 2
+with Session() as ps:
+    descriptor = ps.ActionDescriptor
+    idplacedLayerConvertToLayers = ps.app.stringIDToTypeID("placedLayerConvertToLayers")
+    ps.app.executeAction(idplacedLayerConvertToLayers, descriptor)
