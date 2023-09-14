835f626617413c90cc286bfd2c65230d5079eb72
longhao <hal.long@outlook.com>
2023-09-14 20:50:49 +0800

    chore: add new example for revert changes.
    Signed-off-by: longhao <hal.long@outlook.com>

---
+++ examples/revert_changes.py
@@ -0,0 +1,15 @@
+"""This example demonstrates how to roll back history."""
+
+# Import local modules
+from photoshop import Session
+
+
+with Session() as ps:
+    doc = ps.active_document
+    old_state = doc.activeHistoryState
+    print(old_state.name)
+    doc.artLayers.add()
+    last_state = doc.activeHistoryState
+    print(last_state.name)
+    doc.activeHistoryState = old_state
+    print(doc.activeHistoryState.name)
