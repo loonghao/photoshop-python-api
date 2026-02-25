53965a61b2db2a02234989789d1a38ba57616c45
pappnu <pppskraga@gmail.com>
2026-02-25 08:06:40 +0200

    fix(Notifiers): Access the Photoshop Application object correctly

--- photoshop/api/_notifiers.py
+++ photoshop/api/_notifiers.py
@@ -10,10 +10,6 @@ Examples:
 
 """
 
-# Import built-in modules
-from typing import TYPE_CHECKING
-
-# Import local modules
 from photoshop.api._core import Photoshop
 from photoshop.api._notifier import Notifier
 from photoshop.api.collections import CollectionOfRemovables
@@ -22,20 +18,14 @@ from photoshop.api.collections import CollectionOfRemovables
 class Notifiers(CollectionOfRemovables[Notifier, int]):
     """The `notifiers` currently configured (in the Scripts Events Manager menu in the application)."""
 
-    if TYPE_CHECKING:
-        # Import local modules
-        from photoshop.api.application import Application
-
-        parent: Application
-
     def __init__(self, parent: Photoshop | None = None) -> None:
         super().__init__(type=Notifier, parent=parent)
         self._flag_as_method("add")
 
     def add(self, event: str, event_file: str, event_class: str | None = None) -> Notifier:
-        self.parent.notifiersEnabled = True
+        self.app.application.notifiersEnabled = True
         return Notifier(self.app.add(event, event_file, event_class))
 
     def removeAll(self) -> None:
         self.app.removeAll()
-        self.parent.notifiersEnabled = False
+        self.app.application.notifiersEnabled = False
